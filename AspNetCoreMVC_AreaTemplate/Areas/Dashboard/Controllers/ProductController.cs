ad6acac09aee16a2111f0cb2512c06608e4aacbf
abdurrahman baylan <juns_apo@hotmail.com>
2024-10-05 16:02:58 +0300

    ds

---
+++ AspNetCoreMVC_AreaTemplate/Areas/Dashboard/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC_AreaTemplate.Areas.Dashboard.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
